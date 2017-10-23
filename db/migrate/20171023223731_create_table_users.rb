b4f43d961c297d2189759784266dbd62e5da90bc
Justin Daniel <justinddaniel@gmail.com>
2017-10-23 22:40:24 +0000

    automatically backed up by learn

--- db/migrate/20171023223731_create_table_users.rb
+++ db/migrate/20171023223731_create_table_users.rb
@@ -1,4 +1,8 @@
 class CreateTableUsers < ActiveRecord::Migration
   def change
+    create_table :users do |u|
+      u.string :username
+      u.string :password
+      u.float :balance
   end
 end
