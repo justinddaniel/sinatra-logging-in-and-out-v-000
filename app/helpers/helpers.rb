2c804586811d5ceeedfdb8a813e748fc560cc386
Justin Daniel <justinddaniel@gmail.com>
2017-10-23 23:36:20 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,7 +3,7 @@ class Helpers
 
   def self.current_user(session) 
     binding.pry
-    User.find_by(user_id: session[:id])
+    User.find_by(user_id: session[:user_id])
   end
 
   def self.is_logged_in?(session)
